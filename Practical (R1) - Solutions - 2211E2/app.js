c2db4220e2e08fcffb227eb6036f0ef01a4bed56
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-04 17:34:34 +0500

    Practical (R1) - Solutions - 2211E2

---
+++ Practical (R1) - Solutions - 2211E2/app.js
@@ -0,0 +1,77 @@
+// let n1=+prompt("Enter first number")
+// let n2=+prompt("Enter first number")
+// let n3=+prompt("Enter first number")
+
+// let result=n1*n2*n3
+
+// if(result<0){
+//     alert("negative")
+// }
+// else if(result>0){
+//     alert("positive")
+// }
+
+// var str=prompt("Enter a string")
+// for(let i=str.length-1;i<=0;i--){
+//     document.write(str[i])
+// }
+
+// var country=["Pakistan","India","England"]
+
+// for(let i=0;i<country.length;i++){
+//     if(arr[i].length>arr[i+1]){
+//         alert(arr[i]+"is the largest")
+//     }
+// }
+
+// for(let i=0;i<country.length;i++){
+//     document.write(country[i]+"<br>")
+// }
+
+// let num=+prompt("Enter a number")
+// let arr=[34,45,7,8,1,9,89,108]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>num){
+//         document.write(arr[i]+"<br>")
+//     }
+// }
+// var str=prompt("Enter any string")
+
+// var check=str.toLowerCase()
+
+// if(str===check){
+//     alert("yes")
+// }
+// else{
+//     alert("no")
+// }
+
+// for(let i=0;i<151;i++){
+//     if(i%2===0){
+//         document.write(i+" is even"+"<br>")
+//     }
+//     else if(i%2!==0){
+//         document.write(i+" is odd"+"<br>")
+
+//     }
+// }
+// sum=0
+// for(let i=0;i<1001;i++){
+//     if(i%3===0&&i%5===0){
+// sum=sum+i;
+//     }
+// }
+// document.write(sum)
+
+// let arr=[125,"Johnny",4.25,false,[],true,true,2586]
+// for(let i=0;i<arr.length;i++){
+//     document.write(typeof(arr[i])+"<br>")
+// }
+// let count=0
+// var str=prompt("Enter a string")
+// for(let i=0;i<str.length;i++){
+//     if(str[i].toLowerCase()==="a"||str[i].toLowerCase()==="e"||str[i].toLowerCase()==="i"||str[i].toLowerCase()==="o"||str[i].toLowerCase()==="u")
+//     count=count+1
+// }
+
+// document.write("There are "+count+" Vowels in "+str)
